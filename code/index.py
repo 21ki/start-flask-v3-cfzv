b8c1987b63aa37014dbb330dfc24fc12943ab289
21ki <21kixc@gmail.com>
2024-07-11 10:21:54 +0800

    code

--- code/index.py
+++ code/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import arrow
 import chinese_calendar as cc
-import datetime
+from datetime import datetime
 import pytz
 
 app = Flask(__name__)
@@ -27,43 +27,41 @@ def hello_world(path):
 
     print("FC Invoke End RequestId: " + requestId)
     return response
+def get_cst_time():
+    """
+    获取并转换服务器当前时间为中国标准时间（CST）。
+    """
+    local_time = datetime.now()  # 获取系统本地时间
+    cst_timezone = pytz.timezone('Asia/Shanghai')  # CST 时区为中国标准时间
+    cst_time = local_time.astimezone(cst_timezone)  # 将本地时间转换为 CST 时间
+    return cst_time
 
 @app.route('/api/is_workday', methods=['GET', 'POST'])
 def is_workday():
-    date_str = None
-    if request.method == 'POST':
-        if request.data:
-            try:
-                data = request.get_json()
-                date_str = data.get('date')
-            except:
-                return jsonify({"error": "Invalid JSON data"}), 400
-    else:  # GET request
+    if request.method == 'GET':
         date_str = request.args.get('date')
+    elif request.method == 'POST':
+        data = request.get_json()
+        date_str = data.get('date') if data else None
 
     # 获取服务器当前时间并转换成 CST
-    server_timezone = pytz.timezone('America/New_York')  # 服务器时区为美国东部时区
-    cst_timezone = pytz.timezone('Asia/Shanghai')  # CST 时区为中国标准时间
-    server_time = datetime.datetime.now(server_timezone).astimezone(cst_timezone).date()
+    server_time = get_cst_time().date()
 
     # 如果没有提供日期参数，则使用服务器当前时间
     if not date_str:
         date = server_time
     else:
         try:
-            date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+            date = datetime.strptime(date_str, '%Y-%m-%d').date()
         except ValueError:
             return jsonify({"error": "Invalid date format. Use YYYY-MM-DD"}), 400
 
     is_workday = cc.is_workday(date)
-    
-    # 调试语句，确认日期是否正确转换成中国时区
-    print("Server date and time in CST (China Standard Time):", datetime.datetime.now(datetime.timezone.utc).astimezone(pytz.timezone('Asia/Shanghai')))
-
     return jsonify({
         "date": date.isoformat(),
         "is_workday": is_workday
     })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000)
